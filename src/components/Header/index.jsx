5335f0a41ebfd38200f3704428fa5d1ab0637040
Franki Callard <francescalily@outlook.com>
2023-09-05 10:15:25 +0100

    home page set up

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 const styles = ({ isActive }) => ({
   textDecoration: isActive ? "underline" : "none",
+  marginRight: "40px",
+  marginTop: "40px",
 });
 
 export default function Header() {
   return (
     <main>
       <header>
-        <nav>
+        <nav className="nav-bar">
           <NavLink to="/" style={styles}>
             Home
           </NavLink>
